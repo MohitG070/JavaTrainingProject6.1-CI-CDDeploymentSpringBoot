86602a646484c3a110f6b1d0b1bc5f72d3b6aee4
Mohit Gangwar <mohitgangwar070@gmail.com>
2025-07-26 01:18:38 +0530

    Springboot Project

---
+++ src/main/java/com/main/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com")
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+		System.out.println("Springboot Application is up!!");
+	}
+
+}
